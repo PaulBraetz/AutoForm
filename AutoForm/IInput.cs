8929024bf45d4d09e687ca678d71cc1858a55ac1
Brätz, Paul <paul.braetz@th-wildau.de>
2022-08-02 17:22:21 +0200

    Add project files.

---
+++ AutoForm/IInput.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace AutoForm
+{
+	public interface IInput<T> : IComponent, IHandleEvent, IHandleAfterRender
+	{
+		[Parameter]
+		public T Value { get; set; }
+		[Parameter]
+		public EventCallback<T> ValueChanged { get; set; }
+	}
+}
---
+++ AutoForm/IInputContainer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoForm
+{
+	public interface IInputContainer
+	{
+		public IInput<T> GetInputFor<T>();
+	}
+	public sealed class InputContainer : IInputContainer
+	{
+		public IInput<T> GetInputFor<T>()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
